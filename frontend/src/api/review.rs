c4f63c94c86af828a213b8f67b76fc1f5d586965
carlobortolan <carlobortolan@gmail.com>
2023-05-28 01:40:44 +0200

    Revert "Updated headers"
    This reverts commit f8eaa497a950a004ad123cfeef9ded1607c990a0.

--- frontend/src/api/review.rs
+++ frontend/src/api/review.rs
@@ -1,6 +1,6 @@
 use entity::review;
 use reqwasm::http::Request;
-use shared::REVIEWS_BY_WATCH_ROUTE;
+use shared::REVIEWS_BY_BEER_ROUTE;
 use std::error::Error;
 
 use super::get_api_url;
@@ -9,7 +9,7 @@ pub async fn add_review(
     watch_id: i32,
     review: &review::Model,
 ) -> Result<review::Model, Box<dyn Error>> {
-    let endpoint = REVIEWS_BY_WATCH_ROUTE.replace("{watch_id}", &watch_id.to_string());
+    let endpoint = REVIEWS_BY_BEER_ROUTE.replace("{watch_id}", &watch_id.to_string());
     let url = get_api_url(&endpoint);
     let as_js_value =
         serde_wasm_bindgen::to_value(review).expect("the review to be translatable to a jsvalue");
--- frontend/src/api/watch.rs
+++ frontend/src/api/watch.rs
@@ -1,20 +1,16 @@
-use std::error::Error;
-
-use entity::review::Model as Review;
-use entity::watch::Model as Watch;
-use reqwasm::http::Request;
-use shared::{ApiQueryParams, WATCHES_ROUTE};
-
 use super::get_api_url;
+use entity::watch::Model as Watch;
+use entity::review::Model as Review;
+use reqwasm::http::Request;
+use shared::{ApiQueryParams, BEERS_ROUTE};
+use std::error::Error;
 
 pub async fn get_watches() -> Result<Vec<Watch>, Box<dyn Error>> {
     println!("STARTED get_watches");
 
-    let url = get_api_url(WATCHES_ROUTE);
+    let url = get_api_url(BEERS_ROUTE);
 
     let watches = Request::get(url.as_str())
-        .header("Origin", "https://ticktack-frontend.onrender.com") // Replace with your frontend URL
-        .header("Referer", "https://ticktack-frontend.onrender.com") // Replace with your frontend URL
         .send()
         .await?
         .json::<Vec<Watch>>()
@@ -27,7 +23,7 @@ pub async fn get_watch(
     id: i32,
     queries: Option<ApiQueryParams>,
 ) -> Result<(Watch, Vec<Review>), Box<dyn Error>> {
-    let mut url = get_api_url(&[WATCHES_ROUTE, &id.to_string()].join("/"));
+    let mut url = get_api_url(&[BEERS_ROUTE, &id.to_string()].join("/"));
 
     if let Some(queries) = queries {
         if let Some(expand) = &queries.expand {
@@ -36,9 +32,7 @@ pub async fn get_watch(
     }
 
     let watch = Request::get(url.as_str())
-        .header("Origin", "https://ticktack-frontend.onrender.com") // Replace with your frontend URL
-        .header("Referer", "https://ticktack-frontend.onrender.com") // Replace with your frontend URL
-        .send()()
+        .send()
         .await?
         .json::<(Watch, Vec<Review>)>()
         .await?;
