c1d291705eceb3c31421adb87f89c8192065fb69
carlobortolan <carlobortolan@gmail.com>
2023-05-28 01:40:56 +0200

    Revert "Removed CORS"
    This reverts commit e56344ae60b20d9425b15034d099f1a94630626e.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,11 @@
-use std::io::Error;
-
 use actix_web::{
-    App,
-    HttpServer, web::{self, Data},
+    web::{self, Data},
+    App, HttpServer,
 };
-
 use config::{app_state::AppState, cors::get_cors_config, database::get_db_config, env::Env};
 use routes::{add_review::add_review, all_watches::watches, one_watch_by_id::one_watch_by_id};
 use shared::{BEERS_ROUTE, REVIEWS_BY_BEER_ROUTE, SINGLE_BEER_ROUTE};
+use std::io::Error;
 
 pub mod config;
 mod routes;
@@ -36,16 +34,16 @@ async fn main() -> std::io::Result<()> {
 
     log::info!("Listening on {}:{}", host, port);
     HttpServer::new(move || {
-        //     let cors = get_cors_config(&env);
+        let cors = get_cors_config(&env);
 
         App::new()
             .app_data(Data::new(state.clone()))
-//            .wrap(cors)
+            .wrap(cors)
             .route(BEERS_ROUTE, web::get().to(watches))
             .route(SINGLE_BEER_ROUTE, web::get().to(one_watch_by_id))
             .route(REVIEWS_BY_BEER_ROUTE, web::post().to(add_review))
     })
-        .bind((host, port))?
-        .run()
-        .await
+    .bind((host, port))?
+    .run()
+    .await
 }
